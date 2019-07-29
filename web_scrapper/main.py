94bb4e27707bdf1f970541ff4410606af0973551
KmiiloBerrio <kmiiloberrio@gmail.com>
2019-07-29 10:54:03 -0500

    fetch articles

--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -1,11 +1,17 @@
 import argparse
 import logging
+import re
 
 import news_page_objects as news
 from common import configuration
+from requests import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+is_well_formed_url = re.compile(r'^https?://.+/.+$')  # https://example.com/hello
+is_root_path = re.compile(r'^/.+$')  # /some-text
 
 
 def _news_scraper(news_site_uid):
@@ -15,8 +21,41 @@ def _news_scraper(news_site_uid):
 
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('Article fetched!!')
+            articles.append(article)
+            print(article.title)
+
+    print(len(articles))
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f'Start fetching article {link}')
+
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info=False)
+
+    if article and not article.body:
+        logger.warning('Invalid article. There is no body')
+        return None
+
+    return article
+
+
+def _build_link(host, link):
+    if is_well_formed_url.match(link):
+        return link
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+    else:
+        return f'{host}/{link}'
 
 
 if __name__ == '__main__':
